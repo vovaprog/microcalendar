a8a3994405d2721553b280ce3577bc8787978753
Vladimir Silaev <vovaprog@yandex.ru>
2016-09-16 13:02:24 +0200

    docker

--- docker/prepare_host.sh
+++ docker/prepare_host.sh
@@ -4,6 +4,16 @@
 mkdir -p /home/webuser/docker_data/microcalendar/database
 mkdir -p /home/webuser/docker_data/microcalendar/key
 
+chown webuser:www-data /home/webuser/docker_data
+chown webuser:www-data /home/webuser/docker_data/microcalendar
+chown webuser:www-data /home/webuser/docker_data/microcalendar/database
+chmod u=rwx,g=rx,o-rwx /home/webuser/docker_data
+chmod u=rwx,g=rx,o-rwx /home/webuser/docker_data/microcalendar
+chmod u=rwx,g=rx,o-rwx /home/webuser/docker_data/microcalendar/database
+
+chown root:root /home/webuser/docker_data/microcalendar/key
+
+
 #generate ssl key
 openssl req -new -x509 -days 20000 -keyout server.key -out server.pem
 
@@ -13,5 +23,8 @@ rm server.key.orig
 
 #put key to folder
 mv --force ./server.key /home/webuser/docker_data/microcalendar/key
-mv --force ./server.pem /home/webuser/docker_data/microcalendar/pem
+mv --force ./server.pem /home/webuser/docker_data/microcalendar/key
 
+chmod u=rx,g-rwx,o-rwx /home/webuser/docker_data/microcalendar/key
+chmod u=r,g-rwx,o-rwx /home/webuser/docker_data/microcalendar/key/server.key
+chmod u=r,g-rwx,o-rwx /home/webuser/docker_data/microcalendar/key/server.pem
